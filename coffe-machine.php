c3a9178cb83c13ea8d2c0915da9a76e2d141f4eb
Roberta <97352032+robertaak@users.noreply.github.com>
2022-06-15 18:50:42 +0300

    Add files via upload

--- coffe-machine.php
+++ coffe-machine.php
@@ -8,22 +8,44 @@ $wallet = [
     20 => 10,
     50 => 10,
     100 => 10,
-    200 => 10
-];
-$choice = [
-    'Americano: 1' => 300,
-    'Flat White: 2' => 350,
-    'Latte: 3' => 450,
-    'Cappuccino: 4' => 450,
-    'Hot Chocolate: 5' => 200
+    200 => 2
 ];
 
-foreach ($choice as $item => $price)
+$drink1 = new stdClass();
+$drink1->index = '1';
+$drink1->name = 'Americano';
+$drink1->price = 300;
+
+$drink2 = new stdClass();
+$drink2->index = '2';
+$drink2->name = 'Flat white';
+$drink2->price = 400;
+
+$drink3 = new stdClass();
+$drink3->index = '3';
+$drink3->name = 'Cappuccino';
+$drink3->price = 450;
+
+$drink4 = new stdClass();
+$drink4->index = '4';
+$drink4->name = 'Latte';
+$drink4->price = 500;
+
+$drink5 = new stdClass();
+$drink5->index = '5';
+$drink5->name = 'Hot chocolate';
+$drink5->price = 200;
+
+$menu = [$drink1, $drink2, $drink3, $drink4, $drink5];
+
+echo "Menu: ";
+foreach ($menu as $drink)
 {
-    echo "$item ($price) ";
+    echo "$drink->name,price: $drink->price; ";
 }
 echo PHP_EOL;
 
+echo "Your wallet: ";
 foreach ($wallet as $coin => $amount)
 {
     echo "$coin ($amount) ";
@@ -33,24 +55,53 @@ echo PHP_EOL;
 $insertedAmount = 0;
 
 while (true) {
-    $insertedCoin = (int) readline ('Insert coin ');
-    if ($insertedCoin == null) {
-        $drink = (int)readline('Choose drink ');
+    $insertedCoin = (int)readline('Insert coins: ');
 
+    if ($insertedCoin == null) {
+        $choice = readline('Choose drink: ');
+        foreach($menu as $drink) {
+            if ($choice === $drink->index) {
+                if($insertedAmount < $drink->price) {
+                    echo "Please insert enough money!".PHP_EOL;
+
+                } else {
+                    echo "$drink->name is being prepared".PHP_EOL;
+                    $change = $insertedAmount - $drink->price;
+                    function getChange(int $change): array
+                    {
+                        $denominations = [1, 2, 5, 10, 20, 50, 100, 200];
+                        $result = [];
+
+                        while ($change > 0) {
+                            $coin = array_pop($denominations);
+                            $count = intdiv($change, $coin);
+                            $change -= $count * $coin;
+                            if ($count > 0) {
+                                $result[] = "$coin($count)";
+                            }
+                        }
+                        return $result;
+                    }
+                    die("Please take your change. You change: " . implode(',', getChange($change)) .PHP_EOL);
+                }
+
+            }
+        }
+    }
 
     if (!isset($wallet[$insertedCoin])) {
-        echo 'Enter valid coin '.PHP_EOL;
+        echo 'Enter valid coin ' . PHP_EOL;
         continue;
     }
 
     if ($wallet[$insertedCoin] <= 0) {
-        echo 'Insufficient funds';
-        exit;
+        echo 'You do not have that coin anymore!';
+        echo PHP_EOL;
+        continue;
     }
 
     $wallet[$insertedCoin] -= 1;
     $insertedAmount += $insertedCoin;
-    echo "Balance: $insertedAmount".PHP_EOL;
+    echo "Balance: $insertedAmount" . PHP_EOL;
 }
-
 
